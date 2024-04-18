7309ab8d53a6833c749d483e36ad6d37d181f1c4
jeffhaha101 <2021100951bp@psu.palawan.edu.ph>
2024-04-18 22:04:56 +0800

    added org_list

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView
+from studentorg.views import HomePageView, OrganizationList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('organization_list', OrganizationList.as_view(), name='organization-list'),
 ]
